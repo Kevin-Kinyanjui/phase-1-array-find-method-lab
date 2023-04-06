a899421c85d95404f97201e8d4639424afec06d4
Kevin-Kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-04-06 18:58:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+ function superbowlWin(array) {
+       if (array.result === "W"){
+            return array
+       } else {
+          console.log(undefined)
+       }
+   }
+
+console.log(record.find(superbowlWin))
+
+
